92594bf375cf06d14f8d04f8814df02c1a371cb1
dillzhang <d1998z@gmail.com>
2020-05-08 01:50:34 -0400

    finished five stages

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/console', function(req, res, next) {
-  res.render('console', { title: 'Control Receiver' });
+  res.render('console', { title: 'Space Ship' });
 });
 
 module.exports = router;
--- socketHandler.js
+++ socketHandler.js
@@ -6,6 +6,11 @@ function handleIo(io) {
             console.log(`${socket.id}'s input: ${data}`);
             socket.broadcast.emit('broadcast', data);
         });
+
+        socket.on('response', (data) => {
+            console.log(`${socket.id}'s response: ${data}`);
+            socket.broadcast.emit('output', data);
+        });
     });
 }
 
